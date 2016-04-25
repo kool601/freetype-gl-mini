f93bddc9e06d0e07c8ce7e75d71be3c494fd5877
Luke Iannini <lukexi@me.com>
2016-04-25 14:07:02 -0700

    Split out text-gl into its own repo

--- src/Graphics/GL/TextBuffer.hs
+++
@@ -1,8 +0,0 @@
-module Graphics.GL.TextBuffer (module Exports) where
-
-import Graphics.GL.TextBuffer.Input as Exports
-import Graphics.GL.TextBuffer.Mouse as Exports
-import Graphics.GL.TextBuffer.Metrics as Exports
-import Graphics.GL.TextBuffer.Render as Exports
-import Graphics.GL.TextBuffer.TextBuffer as Exports
-import Graphics.GL.TextBuffer.Types as Exports
